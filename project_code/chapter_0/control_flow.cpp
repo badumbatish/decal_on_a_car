7ebe09a6cc7cd96d48d4846ba38bad4c3ff933b6
Jasmine T <tanghocle456@gmail.com>
2023-10-10 01:07:12 -0700

    Add control flow section

---
+++ project_code/chapter_0/control_flow.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Jasmine Tang on 10/10/23.
+//
+
+#include <iostream>
+
+int main() {
+    std::cout << "Using for-loop, we start iterating: " << std::endl;
+    for (int i = 0; i < 5; i++) {
+        std::cout << "This is iteration " << i << std::endl;
+    }
+
+    std::cout << std::endl; // Output a newline
+    int count = 0;
+    std::cout << "Using while-loop, we start iterating: " << std::endl;
+    while (count < 5) {
+        std::cout << "This is iteration " << count << std::endl;
+        count++;
+    }
+
+    std::cout << std::endl;
+
+    int x = 10;
+
+    if (x > 5) {
+        std::cout << "x is greater than 5" << std::endl;
+    } else {
+        std::cout << "x is not greater than 5" << std::endl;
+    }
+
+    return 0;
+}
